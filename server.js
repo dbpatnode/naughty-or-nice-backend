8502bf518f98235c93a6dd6860f473d186b38f16
dbpatnodee <dbpatnode@hotmail.com>
2021-02-11 12:06:23 -0800

    mongoDB added

--- server.js
+++ server.js
@@ -10,12 +10,42 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to Naughty or Nice. View Santas List. Add people to his list as well."});
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
 // listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+
+// var app = express();
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
+
+// var data = require('./movies.js');
+
+// app.use('/movies',data);
+
+// app.listen(8080);
