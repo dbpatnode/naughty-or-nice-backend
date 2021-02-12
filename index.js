9d7adf64dbb98e12b618b2c55e0ff965e6edb377
dbpatnodee <dbpatnode@hotmail.com>
2021-02-11 17:38:43 -0800

    got everything up to put working on backend

--- index.js
+++ index.js
@@ -17,17 +17,57 @@ app.post("/santaslist", async(req,res) => {
 
         res.json(newListItem.rows[0])
     } catch (err){
-        console.log(err.message)
+        console.error(err.message)
     }
 })
 
-//get all list items
+//get all list items (GET)
+
+app.get("/santaslist", async(req, res) => {
+    try {
+        const allListItems = await pool.query("SELECT * FROM listitems")
+
+        res.json(allListItems.rows)
+    } catch(err){
+        console.error(err.message)
+    }
+})
 
 // get a single list item
 
+app.get("/santaslist/:id", async(req, res) => {
+    try {
+        const {id} = req.params
+         const listItem = await pool.query("SELECT * FROM listitems WHERE listitem_id = $1", [id])
+       
+         res.json(listItem.rows[0])
+    } catch(err){
+        console.error(err.message)
+    }
+})
 //update a  list item
 
+app.put("/santaslist/:id", async(req, res) => {
+    try {
+        const {id} = req.params
+        const { name, street, city, state, zipcode, naughty, nice } = req.body
+        const updateItem = await pool.query("UPDATE listitems SET (name, street, city, state, zipcode, naughty, nice) = ($1, $2, $3, $4, $5, $6, $7) WHERE listitem_id = $8", [name, street, city, state, zipcode, naughty, nice, id])
+
+        res.json("santaslist was updated")
+    } catch(err){
+        console.error(err.message)
+    }
+})
+
 //delete a list item
+
+// app.destroy("/santaslist/:id", async(req, res) => {
+//     try {
+//         console.log(req.params)
+//     } catch(err){
+//         console.error(err.message)
+//     }
+// })
 
 app.listen(5000, () => {
     console.log("Server has been started on port 5000")
